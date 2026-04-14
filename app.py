c66bf2bd69c847f64135b07f2c88a01b9e46e371
DOSull <david@southosullivan.com>
2026-04-14 22:56:34 +1200

    reverted back to 0.0.7.19

--- app.py
+++ app.py
@@ -1079,7 +1079,7 @@ def setup_tilings_dictionary():
 @app.cell
 def _(centred, mo):
     mo.vstack([mo.image(src="mw.png").style(centred),
-               mo.md(f"<p style='font-style:italic;text-align:center;line-height:1.2em;' title='Weaving maps of complex data'>2026.04.14 built with <a href='https://github.com/DOSull/weaving-space' target='_blank'>weavingspace 0.0.7.43</a> and <a href='https://marimo.io'>marimo 0.23.1</a></p>")]).center()
+               mo.md(f"<p style='font-style:italic;text-align:center;line-height:1.2em;' title='Weaving maps of complex data'>2026.04.14 built with <a href='https://github.com/DOSull/weaving-space' target='_blank'>weavingspace 0.0.7.19</a> and <a href='https://marimo.io'>marimo 0.23.1</a></p>")]).center()
     return
 
 
